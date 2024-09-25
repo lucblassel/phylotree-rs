2871134bfa0b591789ed9c47b620af3883c879fb
Luc Blassel <luc.blassel@gmail.com>
2024-09-25 18:15:16 +0200

    Switching back to 1D lower triangular array Due to performance regressions when computing DMs from a tree with 2d arrays

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,6 +1,11 @@
 use criterion::{criterion_group, criterion_main, PlotConfiguration};
 use criterion::{BenchmarkId, Criterion};
 
+use itertools::Itertools;
+use ndarray::{Array2, Axis};
+use ndarray_rand::rand_distr::Uniform as UniformND;
+use ndarray_rand::RandomExt;
+use phylotree::distance::DistanceMatrix;
 use phylotree::{distr::Distr::Uniform, generate_tree};
 
 /// Measure how distance matrix extraction scales with tree size
@@ -50,5 +55,47 @@ fn newick_parsing(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, dm_vs_treesize, newick_parsing);
+fn generate_phylip(size: usize) -> String {
+    // Generate random matrix
+    let base = Array2::random((size, size), UniformND::new(0., 1.));
+    let base = base.view();
+    // Make it symmetric
+    let mut sym = &base + &base.t();
+    sym.diag_mut().fill(0.);
+
+    let s = sym
+        .axis_iter(Axis(0))
+        .zip(0..size)
+        .map(|(row, i)| {
+            let vals = row.iter().map(|v| v.to_string()).join("  ");
+            format!("T{i}    {vals}")
+        })
+        .join("\n");
+
+    format!("{size}\n{s}\n")
+}
+
+fn phylip_parsing(c: &mut Criterion) {
+    let plot_config = PlotConfiguration::default().summary_scale(criterion::AxisScale::Logarithmic);
+    let mut group = c.benchmark_group("phylip_parsing");
+    group.plot_config(plot_config);
+
+    for size in [10, 20, 40, 100, 500, 1000, 2000, 5000, 10000].iter() {
+        let phylip = generate_phylip(*size);
+        group.bench_with_input(BenchmarkId::new("Lower", size), size, |bencher, _| {
+            bencher.iter(|| {
+                let _ = DistanceMatrix::<f64>::from_phylip_tril(&phylip);
+            })
+        });
+        if *size <= 1000 {
+            group.bench_with_input(BenchmarkId::new("Strict", size), size, |bencher, _| {
+                bencher.iter(|| {
+                    let _ = DistanceMatrix::<f64>::from_phylip_strict(&phylip, true);
+                })
+            });
+        }
+    }
+}
+
+criterion_group!(benches, dm_vs_treesize, newick_parsing, phylip_parsing);
 criterion_main!(benches);
